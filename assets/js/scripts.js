0b28d886779cf0a907fad53ef8d294eb1ad6c3ba
Jeff.A <kinesthesiac@gmail.com>
2020-12-24 22:14:24 -0800

    image troubleshooting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,10 +7,6 @@ let clicked6 = false;
 let clicked7 = false;
 let clicked8 = false;
 let clicked9 = false;
-<<<<<<< HEAD
-
-=======
->>>>>>> 23e78611489ebd0409c52ea159e3546331073706
 // $(document).ready(function() {
 //    let audioElement = document.createElement('audio');
 //    audioElement.setAttribute('src', 'assets/audio/nice.m4a');
@@ -35,10 +31,9 @@ let clicked9 = false;
 
 // })
 
-<<<<<<< HEAD
 $('#tiffAndWrenna').click(function () {
    setTimeout(function () {
-      $('#tiffAndWrenna').attr('src', 'assets/images/tiffAndWrenna.jpg')
+      $('#tiffAndWrenna').attr('src', './assets/images/tiffAndWrenna.jpg')
       // $('#audio1').append('<audio src="assets/audio/nice.m4a"></audio>')
       // $('#nice').play(nice);
    }, 750);
@@ -111,59 +106,3 @@ $('#jeffAndQueso').click(function () {
 })
 
 
-=======
-$("#tiffAndWrenna").click(function () {
-  setTimeout(function () {
-    $("#tiffAndWrenna").attr("src", "assets/images/tiffAndWrenna.jpg");
-    // $('#audio1').append('<audio src="assets/audio/nice.m4a"></audio>')
-    $("#nice").play(nice);
-  }, 750);
-
-  setTimeout(function () {
-    $("#text1").text("Merry Christmas!  Keep scrolling down...");
-  }, 1500);
-
-  clicked1 = true;
-  console.log(clicked1);
-});
-
-$("#rainbow").click(function () {
-  if (clicked1 == true) {
-    setTimeout(function () {
-      $("#rainbow").attr("src", "assets/images/rainbow.jpg");
-    }, 750);
-
-    setTimeout(function () {
-      $("#text2").text(
-        "I'm so happy that out little family can celebrate all together and cozy and safe."
-      );
-      clicked2 = true;
-    }, 1500);
-  } else {
-    confirm("You missed a present! :*");
-  }
-});
-
-$("#wrenna1").click(function () {
-   if (clicked2 == true) {
-      setTimeout(function)
-   }
-});
-
-$("#jeffAndQueso").click(function () {
-  if (clicked1 == true) {
-    setTimeout(function () {
-      $("#text2").text("We have a present for you!");
-    }, 750);
-
-    setTimeout(function () {
-      $("#jeffAndQueso").attr("src", "assets/images/JeffQueso.jpg");
-    }, 2000);
-    setTimeout(function () {
-      $("#text3").text("Whaaaaat?");
-    }, 2500);
-  } else {
-    confirm("You missed a present!");
-  }
-});
->>>>>>> 23e78611489ebd0409c52ea159e3546331073706
