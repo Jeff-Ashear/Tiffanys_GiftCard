2346a1844627b6954353497956c84d05e6e18c70
Jeff.A <kinesthesiac@gmail.com>
2020-12-24 22:10:45 -0800

    functions and testing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,12 @@
 let clicked1 = false;
 let clicked2 = false;
+let clicked3 = false;
+let clicked4 = false;
+let clicked5 = false;
+let clicked6 = false;
+let clicked7 = false;
+let clicked8 = false;
+let clicked9 = false;
 
 // $(document).ready(function() {
 //    let audioElement = document.createElement('audio');
@@ -26,47 +33,78 @@ let clicked2 = false;
 
 // })
 
-$('#tiffAndWrenna').click(function() {
-   setTimeout(function() {
+$('#tiffAndWrenna').click(function () {
+   setTimeout(function () {
       $('#tiffAndWrenna').attr('src', 'assets/images/tiffAndWrenna.jpg')
       // $('#audio1').append('<audio src="assets/audio/nice.m4a"></audio>')
-      $('#nice').play(nice);
+      // $('#nice').play(nice);
    }, 750);
 
-   setTimeout(function() {
-       $('#text1').text("Merry Christmas!  Keep scrolling down...")
-   }, 1500);
+   setTimeout(function () {
+      $('#text1').text("Merry Christmas!  Keep scrolling down...")
+   }, 2000);
 
-   clicked1 = true;
-   console.log(clicked1);
-})
+    clicked1 = true;
+   })
 
-$('#rainbow').click(function() {
-   setTimeout(function() {
-      $('#rainbow').attr('src', 'assets/images/rainbow.jpg')
-   }, 750);
-
-   setTimeout(function() {
-      $('#text2').text("I'm so happy that out little family can celebrate all together and cozy and safe.")
-   }, 1500);
-
-})
-
-$('#jeffAndQueso').click(function() {
+$('#rainbow').click(function () {
    if (clicked1 == true) {
-   setTimeout(function() {
-      $("#text2").text("We have a present for you!")
-   }, 750)
+      setTimeout(function () {
+         $('#rainbow').attr('src', 'assets/images/rainbow.jpg')
+      }, 750);
 
-   setTimeout(function() {
-      $('#jeffAndQueso').attr('src', 'assets/images/JeffQueso.jpg')
-   }, 2000)
-   setTimeout(function() {
-      $("#text3").text("Whaaaaat?")
-   }, 2500)
-} else {
-   confirm("You missed a present!")
-}
+      setTimeout(function () {
+         $('#text2').text("I'm so happy that our little family can celebrate all together and cozy and safe.")
+      }, 2000);
+   } else {
+      confirm("You missed a present!");
+   }
+
+   clicked2 = true;
+})
+
+$('#wrenna1').click(function() {
+   if(clicked2 == true){
+      setTimeout(function() {
+         $('#text3').text('Wrenna loves you sooooo much. :"')
+      }, 750);
+
+      setTimeout(function() {
+         $('#wrenna1').attr('src', 'assets/images/wrenna1.jpg')
+      }, 2000);
+   } else {
+      confirm("You mised a present!")
+   }
+
+   clicked3 = true;
+});
+
+$('#jeffAndWrenna').click(function() {
+
+});
+
+
+
+
+
+
+
+
+$('#jeffAndQueso').click(function () {
+   if (clicked1 == true) {
+      setTimeout(function () {
+         $("#text2").text("We have a present for you!")
+      }, 750)
+
+      setTimeout(function () {
+         $('#jeffAndQueso').attr('src', 'assets/images/JeffQueso.jpg')
+      }, 2000)
+      setTimeout(function () {
+         $("#text3").text("Whaaaaat?")
+      }, 2500)
+   } else {
+      confirm("You missed a present!")
+   }
 })
 
 
